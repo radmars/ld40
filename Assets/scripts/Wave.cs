1db77c460109c959323a51fb6a8271796e06a808
Erik Driscoll <edriscoll@softheon.com>
2017-12-02 17:20:23 -0500

    Enemy wave spawner, put in time of enemy (we could make prefabbed rails) number of dudes and spawn rate and let it rip

---
+++ Assets/scripts/Wave.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Wave
+{
+    public Transform baddie;
+    public int amount;
+    public float rate;
+}
---
+++ Assets/scripts/baddie/WaveSpawner.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public enum SpawnState { SPAWNING, WAITING, COUNTING };
+
+    public Wave[] waves;
+    private int nextWave = 0;
+    public float timeBetweenWaves = 5f;
+    public float waveCountDown = 0f;
+    private SpawnState state = SpawnState.COUNTING;
+
+    void Start()
+    {
+        waveCountDown = timeBetweenWaves;
+    }
+
+    void Update()
+    {
+
+        if(state == SpawnState.WAITING)
+        {
+            return;
+        }
+
+        if (waveCountDown <= 0)
+        {
+            if(state != SpawnState.SPAWNING)
+            {
+                StartCoroutine(SpawnWave(waves[nextWave]));
+            }
+        }
+        else
+        {
+            waveCountDown -= Time.deltaTime;
+        }
+    }
+
+    IEnumerator SpawnWave (Wave wave)
+    { 
+        state = SpawnState.SPAWNING;
+
+        for(int i = 0; i< wave.amount; i++)
+        {
+            SpawnBaddie(wave.baddie);
+            yield return new WaitForSeconds(1f / wave.rate);
+        }
+
+        yield break;
+    }
+
+    void SpawnBaddie(Transform baddie)
+    {
+        // TODO: Replace with pulling from baddie pool
+        Instantiate(baddie, transform.position, transform.rotation);
+    }
+}
