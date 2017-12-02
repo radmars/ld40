98bd6c243072a6c55712cd8f846a458021e16663
Eugene Marcotte <emarcotte@gmail.com>
2017-12-02 13:30:23 -0500

    Setup some layers and some collision ignores rules

---
+++ Assets/scripts/level/ScenePhysicsConstructor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ScenePhysicsConstructor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		Physics.IgnoreLayerCollision(LayerMask.NameToLayer("Baddies"), LayerMask.NameToLayer("Bullets"));
+		Physics.IgnoreLayerCollision(LayerMask.NameToLayer("Baddies"), LayerMask.NameToLayer("Level"));
+		Physics.IgnoreLayerCollision(LayerMask.NameToLayer("Level"), LayerMask.NameToLayer("Bullets"));
+	}
+}
--- Assets/scripts/player/PlayerInputController.cs
+++ Assets/scripts/player/PlayerInputController.cs
@@ -1,16 +1,20 @@
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-public class PlayerInputController : MonoBehaviour {
+public class PlayerInputController : MonoBehaviour
+{
 
 	private Rigidbody body;
+
 	// Use this for initialization
-	void Start () {
+	void Start()
+	{
 		body = GetComponent<Rigidbody>();
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
 		var inputDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
 		body.AddForce(inputDirection, ForceMode.VelocityChange);
 	}
