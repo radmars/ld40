3c366067fc8f52fa55965e874680a5cbe70f485b
Eugene Marcotte <emarcotte@gmail.com>
2017-12-02 12:44:18 -0500

    Some rough player input and screen edge collision

---
+++ Assets/scripts/player/PlayerInputController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerInputController : MonoBehaviour {
+
+	private Rigidbody body;
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		var inputDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+		body.AddForce(inputDirection, ForceMode.VelocityChange);
+	}
+}
