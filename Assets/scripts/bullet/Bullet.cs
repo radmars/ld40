d0808a8ecffa02266d2e3b7fcae014d9e4defc6d
Eugene Marcotte <emarcotte@gmail.com>
2017-12-03 08:39:17 -0500

    player is movin around and is a plane

--- Assets/scripts/bullet/Bullet.cs
+++ Assets/scripts/bullet/Bullet.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Bullet : MonoBehaviour
 {
 	public float speed;
 	public Vector3 direction;
-	public Rigidbody2D body;
+	private Rigidbody2D body;
 
 	public void Start()
 	{
