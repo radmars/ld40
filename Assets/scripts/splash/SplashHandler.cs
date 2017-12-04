4f268409ca6e68e50dfc7a4b3003c3236201d186
Adhesion <adhesionmusic@gmail.com>
2017-12-04 01:05:43 -0500

    Title screen sound fixes etc

--- Assets/scripts/splash/SplashHandler.cs
+++ Assets/scripts/splash/SplashHandler.cs
@@ -1,15 +1,30 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SplashHandler : MonoBehaviour
 {
 	public string nextScene;
+    public AudioSource titleSound;
 
 	void Update()
 	{
 		if (Input.anyKey)
 		{
-			SceneManager.LoadScene("emarcotte-test");
+            StartCoroutine(StartGame());
 		}
 	}
+
+    private IEnumerator StartGame() {
+        titleSound.Play();
+
+        while (titleSound.isPlaying)
+        {
+            yield return null;
+        }
+
+		SceneManager.LoadScene("emarcotte-test");
+    }
 }
