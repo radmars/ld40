69eefbbdd59d899c8288f3a811e4e375ac510b6a
Eugene Marcotte <emarcotte@gmail.com>
2017-12-03 21:26:31 -0500

    Size and mass increases

--- Assets/scripts/player/Ball.cs
+++ Assets/scripts/player/Ball.cs
@@ -6,9 +6,14 @@ using UnityEngine.UI;
 public class Ball : MonoBehaviour
 {
     public Text scoreText;
+	public float massPerBaddie = 1;
+	public float scalePerBaddie = 1.15f;
 	List<Baddie> attached;
     long score = 0;
-    
+	public GameObject geometry;
+	public Rigidbody2D body;
+	public new BoxCollider2D collider;
+
 	public void Start()
 	{
 		attached = new List<Baddie>();
@@ -20,6 +25,9 @@ public class Ball : MonoBehaviour
 		attached.Add(b);
 		b.transform.parent = transform;
 		b.transform.position = b.transform.position;
+		geometry.transform.localScale = geometry.transform.localScale * scalePerBaddie;
+		collider.size = collider.size * scalePerBaddie;
+		body.mass += massPerBaddie;
 
         //TODO COME UP WITH REAL SCORING (Score based on pickups on number of baddies attacahed?)
         UpdateScore();
