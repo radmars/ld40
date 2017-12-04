a5a8113429219e49c65ddd7a3f17eb892cd6875a
Erik Driscoll <edriscoll@softheon.com>
2017-12-03 23:31:28 -0500

    Fix minor issues with tethered ball and baddies

--- Assets/scripts/player/Ball.cs
+++ Assets/scripts/player/Ball.cs
@@ -80,6 +80,7 @@ public class Ball : MonoBehaviour
 		body.AddForce(new Vector3(push.x, push.y));
 		body.drag = 0;
 		transform.parent = null;
+        tethered = false;
 	}
 
 	private void OnTriggerEnter2D(Collider2D collision)
@@ -98,6 +99,7 @@ public class Ball : MonoBehaviour
 			Visible = false;
             foreach(Baddie baddie in attached)
             {
+                if(!tethered)
                 baddie.Freshen();
             }
 		}
