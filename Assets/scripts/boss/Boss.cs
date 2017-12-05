343b5798747b0904b813743ece52528717207e0d
Adhesion <adhesionmusic@gmail.com>
2017-12-04 19:48:48 -0500

    Basic boss shooting, too hard

--- Assets/scripts/boss/Boss.cs
+++ Assets/scripts/boss/Boss.cs
@@ -14,8 +14,21 @@ public class Boss : MonoBehaviour
     public Slider healthSlider;
 	public int startingHitPoints;
     public AudioSource explosionSound;
+    public AudioSource shootSound;
 
-	protected List<Collider2D> weakSpots;
+    public BulletPool bulletPool;
+    public Bullet bulletPrefab;
+    public PlayerInputController target;
+    private float rateOfFire = 0.15f;
+    public float lastShot = 0;
+
+    private bool isActive = false;
+    private uint bulletWaveMax = 25;
+    private uint bulletWaveCurrent = 0;
+    private bool bulletFocus = true;
+    private float sineScaler = 5.0f;
+
+    protected List<Collider2D> weakSpots;
 
 	public void Start()
 	{
@@ -24,7 +37,61 @@ public class Boss : MonoBehaviour
 		healthSlider.value = hitPoints;
 	}
 
-	public void OnCollisionEnter2D(Collision2D collision)
+    private float GetROF() {
+        return bulletFocus ? rateOfFire : rateOfFire / 2.0f;
+    }
+
+    public void Update() {
+        if (lastShot + GetROF() < Time.time && isActive)
+        {
+            lastShot = Time.time;
+            Shoot();
+        }
+    }
+
+    public void Shoot() {
+        if (bulletPool == null)
+        {
+            bulletPool = GameObject.FindObjectOfType<BulletPool>();
+        }
+
+        if (bulletWaveCurrent >= bulletWaveMax * 1.5f)
+        {
+            bulletWaveCurrent = 0;
+            bulletFocus = !bulletFocus;
+        }
+        else if (bulletWaveCurrent >= bulletWaveMax)
+        {
+            bulletWaveCurrent++;
+            return;
+        }
+
+        var bullet = bulletPool.GetInstance(bulletPrefab);
+        bullet.transform.position = transform.position;
+
+        if (target)
+        {
+            if (bulletFocus)
+            {
+                bullet.direction = (target.transform.position - bullet.transform.position).normalized;
+            }
+            else
+            {
+                double progress = (double)(bulletWaveCurrent) / (double)(bulletWaveMax);
+                bullet.direction = new Vector3((float)Math.Sin(progress * sineScaler * Math.PI), (float)Math.Cos(progress * sineScaler - 1.0f * Math.PI), 0);
+            }
+        }
+        else
+        {
+            bullet.direction = -(Vector3.up);
+        }
+
+        shootSound.pitch = UnityEngine.Random.Range(0.55f, 0.75f);
+        shootSound.Play();
+        bulletWaveCurrent++;
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
 	{
 		if (collision.collider.gameObject == theBall.gameObject)
 		{
@@ -45,6 +112,7 @@ public class Boss : MonoBehaviour
 
 	private IEnumerator Die()
 	{
+        isActive = false;
         explosionSound.pitch = 0.75f;
         explosionSound.Play();
         healthSlider.gameObject.SetActive(false);
@@ -68,7 +136,7 @@ public class Boss : MonoBehaviour
 		var start = new Vector3(0, 6, 0);
 		var end = new Vector3(0, 2.8f, 0);
 		float startTime = Time.time;
-		float slideTime = 10f;
+		float slideTime = 10.0f;
 
         yield return new WaitUntil(() =>
 		{
@@ -76,6 +144,8 @@ public class Boss : MonoBehaviour
 			transform.position = Vector3.Lerp(start, end, t);
 			return t >= 1.0f;
 		});
+
+        isActive = true;
 	}
 
     private void RefreshHealthBar()
