3bf1f34972d69ef7ea9899ec8b81bc1731f24121
thebrendo <brendon@Brendons-MacBook-Air-2.local>
2017-12-04 20:50:59 -0500

    Added spacebar to release ball

--- Assets/scripts/player/PlayerInputController.cs
+++ Assets/scripts/player/PlayerInputController.cs
@@ -30,7 +30,7 @@ public class PlayerInputController : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		if(Input.GetButtonDown("Fire1"))
+		if(Input.GetButtonDown("Fire1") || Input.GetKeyDown(KeyCode.Space))
 		{
 			Release();
 		}
